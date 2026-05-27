8dcfb722e6c06bb0c783f7c90be5b74e899b812b
eherbepin <emmanuel.herbepin@lilo.org>
2026-05-27 16:45:46 +0200

    Code d'agregation des .md

---
+++ R/insertion_lignes.R
@@ -0,0 +1,117 @@
+#########################################################
+# Utilitaire pour insérer les codes isd et les libellés #
+#########################################################
+
+library(dplyr)
+library(stringr)
+library(readr)
+library(purrr)
+
+# Table de correspondance
+
+df_correspondance <- readxl::read_xlsx(
+  "~/kDrive/Common documents/Drees/Livraison BISOK Mai 2026/documentation_biso_v27_05.xlsx"
+) |>
+  select(id_indicateur, isd, lib_indicateur)
+
+yaml <- c(
+  "---
+  title: \"Documentation BISO\"
+  lang: fr
+  format: 
+   pdf:
+     toc: true
+     number-sections: false
+     colorlinks: true
+     fontsize: 12pt
+     linestretch: 1.2
+     geometry:
+      - margin=2.5cm
+  ---",
+  ""
+)
+
+# Lecture des qmd
+lignes <- c(
+  yaml,
+  map(
+    c(
+      "finess.md",
+      "as.md",
+      "rp.md",
+      "autre.md"
+    ),
+    read_lines
+  ) |>
+    flatten_chr()
+)
+
+# Fonction d'insertion
+
+resultat <- map(
+  seq_along(lignes),
+  function(i) {
+    ligne <- lignes[i]
+
+    # Détection du bloc ID_indicateur
+    if (str_detect(ligne, "^## \\*\\*ID_indicateur :\\*\\*")) {
+      # Extraction des ids
+      ids <- ligne |>
+        str_remove("^## \\*\\*ID_indicateur :\\*\\*\\s*") |>
+        str_split(",\\s*") |>
+        unlist()
+
+      # Recherche des codes ISD
+      codes_isd <- df_correspondance |>
+        filter(id_indicateur %in% ids) |>
+        distinct(isd) |>
+        pull(isd) |>
+        na.omit()
+
+      # Recherche des libellés
+      lib_indicateurs <- df_correspondance |>
+        filter(id_indicateur %in% ids) |>
+        distinct(lib_indicateur) |>
+        pull(lib_indicateur) |>
+        na.omit()
+
+      # Lignes à ajouter
+      ligne_isd <- paste0(
+        "**Code ISD :** ",
+        paste(codes_isd, collapse = ", ")
+      )
+
+      if (length(codes_isd) == 0) {
+        c(
+          ligne,
+          "",
+          "**Libellé de l'indicateur :**",
+          "",
+          paste0("- ", lib_indicateurs)
+        )
+      } else {
+        c(
+          ligne,
+          "",
+          "**Libellé :**",
+          "",
+          paste0("- ", lib_indicateurs),
+          "",
+          ligne_isd
+        )
+      }
+    } else {
+      ligne
+    }
+  }
+) |>
+  unlist()
+
+# ------------------------------------------------------------------
+# Écriture du nouveau qmd
+# ------------------------------------------------------------------
+
+write_lines(
+  resultat,
+  "mon_fichier_enrichi.qmd"
+)
