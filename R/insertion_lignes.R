421ab45999776389c5832ffc7f0d3718a02a67f6
eherbepin <emmanuel.herbepin@lilo.org>
2026-07-15 16:58:26 +0200

    Insertion des liens

--- R/insertion_lignes.R
+++ R/insertion_lignes.R
@@ -111,10 +111,52 @@ resultat <- map(
 ) |>
   unlist()
 
+# Ajout de liens ----
+
+# On ajoute les codes ISD et les libellés des indicateurs
+resultat_liens <- str_replace_all(
+  resultat,
+
+  c(
+    "ID_indicateur" = "ID indicateur",
+    "Finess" = "[Finess](#finess)",
+    "EHPA" = "[EHPA](#enquête-auprès-des-établissements-dhébergement-pour-personnes-âgées)",
+    "FiLoSoFi" = "[FiLoSoFi](#insee-dispositif-fichier-localisé-social-et-fiscal-filosofi)",
+    "RSA" = "[RSA](#rsa)",
+    "APA" = "[APA](#apa)",
+    "Âge" = "[Âge](#age)",
+    "âge" = "[âge](#age)",
+    "Juge des enfants " = "[Juge des enfants](#juge-des-enfants)",
+    "juge des enfants " = "[juge des enfants](#juge-des-enfants)",
+    "Catégories de demandes d'emploi" = "[Catégories de demandes d'emploi](#catégories-des-inscrits-à-france-travail)",
+    "Espérance de vie" = "[Espérance de vie](#espérance-de-vie)",
+    "espérance de vie" = "[espérance de vie](#espérance-de-vie)",
+    "Revenu disponible" = "[Revenu disponible](#revenu-disponible)",
+    "revenu disponible" = "[revenu disponible](#revenu-disponible)",
+    "Unité de consommation" = "[Unité de consommation](#unité-de-consommation)",
+    "unité de consommation" = "[unité de consommation](#unité-de-consommation)",
+    "Unité de Consommation" = "[Unité de Consommation](#unité-de-consommation)",
+    "Intensité de la pauvreté" = "[Intensité de la pauvreté](#intensité-de-la-pauvreté)",
+    "intensité de la pauvreté" = "[intensité de la pauvreté](#intensité-de-la-pauvreté)",
+    "Niveau de vie" = "[Niveau de vie](#niveau-de-vie)",
+    "niveau de vie" = "[niveau de vie](#niveau-de-vie)",
+    "Nombre de personnes du ménage fiscal" = "[Nombre de personnes du ménage fiscal](#nombre-de-personnes-du-ménage-fiscal)",
+    "nombre de personnes du ménage fiscal" = "[nombre de personnes du ménage fiscal](#nombre-de-personnes-du-ménage-fiscal)",
+    "Rapports interdéciles" = "[Rapports interdéciles](#rapports-interdéciles)",
+    "rapports interdéciles" = "[rapports interdéciles](#rapports-interdéciles)",
+    "Pauvreté" = "[Pauvreté](#pauvreté)",
+    "pauvreté" = "[pauvreté](#pauvreté)",
+    "Déciles" = "[Déciles](#déciles)",
+    "déciles" = "[déciles](#déciles)",
+    "Taux de chômage localisés" = "[Taux de chômage localisés](https://www.insee.fr/fr/metadonnees/source/serie/s2107)"
+  )
+)
+
+
 # Écriture du nouveau qmd et production du pdf -----
 
 write_lines(
-  resultat,
+  resultat_liens,
   "documentation_biso.qmd"
 )
 
